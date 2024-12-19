65ab6964ab3a726d31d3e09ec6f0c7162207292f
nabil aguida <47725377+nabilaguida@users.noreply.github.com>
2024-12-19 14:11:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">pts za3im na9ch we habib za3im bsm allah</h1>
+          <h1 className="App-title">pts za3im we habib za3im bsm allah</h1>
         </header>
           <Switch>
                 <Route exact path= "/" render={() => (
